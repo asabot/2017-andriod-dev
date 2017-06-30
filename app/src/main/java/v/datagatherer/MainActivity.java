d1e5f494ed590d393ae573a5d8197f1689699aed
xueyuxy <yu.xue@mail.utoronto.ca>
2017-06-30 14:28:46 -0400

    working connection to the database

--- app/src/main/java/v/datagatherer/MainActivity.java
+++ app/src/main/java/v/datagatherer/MainActivity.java
@@ -22,6 +22,13 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
@@ -183,9 +190,30 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
         textViewThree.setText("Cleared");
 
     }
+   //final TextView mTextView = (TextView) findViewById(R.id.text);
 
     public void uploadData(View view){
-        
+        final TextView textViewThree = (TextView) findViewById(R.id.TextView_3);
+       // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://192.168.1.26:5000";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textViewThree.setText("Response is: "+ response); //.substring(0,500)
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textViewThree.setText("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
 
 
 
